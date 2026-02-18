f76b94f0efc46cc23fe63bf779f6db51f16b5607
Gabriel Dubiela <gabriel.dubiela@hotmail.com>
2026-02-18 13:06:17 -0300

    fix mongocontext and mongodbservice to be compatible

--- VehicleRentalManager/Services/MongoContext.cs
+++ VehicleRentalManager/Services/MongoContext.cs
@@ -1,13 +1,15 @@
 using MongoDB.Driver;
 using MongoDB.Bson;
+using Microsoft.Extensions.Configuration;
 
 public class MongoContext
 {
     public IMongoDatabase Database { get; }
+
     public MongoContext(IConfiguration config)
     {
-        var connectionString = config["MongoDB:ConnectionString"];
-        var dbName = config["MongoDB:Database"];
+        var connectionString = config["MongoDb:ConnectionString"];
+        var dbName = config["MongoDb:DatabaseName"];
 
         var client = new MongoClient(connectionString);
         Database = client.GetDatabase(dbName);
